d8c67bab4926996c6128cfaf8a22d4bd615a6bd6
Aditya Venkata Siva Sai Bhaskar Macharla <63512151+Aditya-Mac@users.noreply.github.com>
2021-06-26 21:00:04 +0530

    Update counter.jsx

--- counter.jsx
+++ counter.jsx
@@ -1,45 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-class Counter extends React.Component {
+class Counter extends Component {
     constructor(props) {
-      super(props);
-      this.state = {
-        count: 0
-      };
-      // Change code below this line
-      this.increment = this.increment.bind(this);
-      this.decrement = this.decrement.bind(this);
-      this.reset = this.reset.bind(this);
-      
-      // Change code above this line
+        super(props);
+        this.state = {
+            count: 0
+        };
+        this.increment = this.increment.bind(this)
+        this.decrement = this.decrement.bind(this)
+        this.reset = this.reset.bind(this)
+        
     }
-    // Change code below this line
-    increment(){
-      this.setState(state => ({
-        count: state.count + 1
-      }));
-    }
-    decrement(){
-      this.setState(state => ({
-        count: state.count - 1
-      }));
-    }
-    reset(){
-      this.setState(state => ({
-        count: 0
-      }));
-    }
-    // Change code above this line
-    render() {
-      return (
-        <div>
-          <button className='inc' onClick={this.increment}>Increment!</button>
-          <button className='dec' onClick={this.decrement}>Decrement!</button>
-          <button className='reset' onClick={this.reset}>Reset</button>
-          <h1>Current Count: {this.state.count}</h1>
-        </div>
-      );
-    }
-};
 
-export default Counter
+    increment() {
+        this.setState({
+            count:this.state.count + 1}
+        )
+    }
+
+    decrement() {
+        this.setState({
+            count:this.state.count - 1}
+        )
+    }
+
+    reset() {
+        this.setState({
+            count:0}
+        )
+    }
+    render() { 
+        return (
+            <div>
+                <button className="btn btn-outline-primary m-2" onClick={this.increment}>Increment</button>
+                <button className="btn btn-outline-secondary m-2" onClick={this.decrement}>Decrement</button>
+                <button className="btn btn-outline-danger m-2" onClick={this.reset}>Reset</button>
+                <h1>{this.state.count}</h1>
+            </div>
+         );
+    }
+}
+ 
+export default Counter;
